c22437f2e16af4de4195b2eac9ebb9a51294e466
Trujun Zhang <wanghao720sn@sina.com>
2020-03-26 17:14:26 +0800

    user profile.

---
+++ src/components/page/userprofile/rightPanel.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <base-material-card
+    class="v-card-profile"
+    avatar="https://demos.creative-tim.com/vue-material-dashboard/img/marc.aba54d65.jpg"
+  >
+    <v-card-text class="text-center">
+      <h6 class="display-1 mb-1 grey--text">
+        CEO / CO-FOUNDER
+      </h6>
+
+      <h4 class="display-2 font-weight-light mb-3 black--text">
+        Alec Thompson
+      </h4>
+
+      <p class="font-weight-light grey--text">
+        Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the back is...
+      </p>
+
+      <v-btn
+        color="success"
+        rounded
+        class="mr-0"
+      >
+        Follow
+      </v-btn>
+    </v-card-text>
+  </base-material-card>
+</template>
